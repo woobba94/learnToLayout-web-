7567a5c4c1887509927250de6c11925d6f45007f
woobba94 <user@email.mail>
2021-11-12 16:13:40 +0900

    add__nav-activeClass

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,14 @@ AOS.init({
     duration: 800,
     anchorPlacement: 'center-bottom'
 });
+
+$(document).ready(function () {
+    $('a').on('click', function (event) {
+        event.preventDefault();
+        $('a').removeClass('active');
+        $(this).addClass('active')
+        var aid = $(this).attr("href");
+        $('html,body').animate({scrollTop: $(aid).offset().top});
+    })
+});
+
